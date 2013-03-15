d67661789aee2a144ed4245f24355d2286a3dca0
James Halliday <mail@substack.net>
2013-03-14 21:20:24 -0700

    updated for variations in reported computed style

--- test/insert.js
+++ test/insert.js
@@ -5,14 +5,25 @@ var css = 'body { background-color: purple; color: yellow; }';
 
 test(function (t) {
     t.plan(4);
+    if (typeof window.getComputedStyle !== 'function') {
+        t.fail('cannot getComputedStyle()');
+    }
     
-    var beforeStyle = window.getComputedStyle(document.body);
-    t.equal(beforeStyle.backgroundColor, 'rgba(0, 0, 0, 0)');
-    t.equal(beforeStyle.color, 'rgb(0, 0, 0)');
+    var before = colors();
+    t.ok(before.bg === 'rgba(0,0,0,0)' || before.bg === 'transparent');
+    t.equal(before.fg, 'rgb(0,0,0)');
     
     insertCss(css);
     
-    var afterStyle = window.getComputedStyle(document.body);
-    t.equal(afterStyle.backgroundColor, 'rgb(128, 0, 128)');
-    t.equal(afterStyle.color, 'rgb(255, 255, 0)');
+    var after = colors();
+    t.equal(after.bg, 'rgb(128,0,128)');
+    t.equal(after.fg, 'rgb(255,255,0)');
 });
+
+function colors () {
+    var style = window.getComputedStyle(document.body);
+    return {
+        bg: style.backgroundColor.replace(/\s+/g, ''),
+        fg: style.color.replace(/\s+/g, '')
+    };
+}
